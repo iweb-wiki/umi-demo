34c71274b10a02e84f476a5a761a3673bebff53c
wang ps <wangps@10.0.0.7>
2020-06-03 00:32:34 +0800

    add page

--- .umirc.ts
+++ .umirc.ts
@@ -1,8 +1,31 @@
 import { defineConfig } from 'umi';
-
 export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
-  routes: [{ path: '/', component: '@/pages/index' }],
+  dva: {
+    immer: true,
+    hmr: false,
+  },
+  routes: [
+    {
+      name: '高级表单',
+      path: '/formadvancedform',
+      component: './FormAdvancedForm',
+    },
+    {
+      name: '分步表单',
+      path: '/formstepform',
+      component: './FormStepForm',
+    },
+    {
+      name: '分步表单',
+      path: '/formstepform',
+      component: './FormStepForm',
+    },
+    {
+      path: '/',
+      component: '@/pages/index',
+    },
+  ],
 });
